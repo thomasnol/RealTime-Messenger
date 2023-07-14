61627117b54e997e56e0eadcd7d18e8e9e273c07
Thomas.N <thomasnolasque@cmail.carleton.ca>
2023-07-14 15:29:05 -0400

    Fixed getMovies

--- express-backend-app/controllers/movie-ctrl.js
+++ express-backend-app/controllers/movie-ctrl.js
@@ -93,17 +93,15 @@ getMovieById = async (req, res) => {
 }
 
 getMovies = async (req, res) => {
-    await Movie.find({}, (err, movies) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+    try {
+    const movie = await Movie.find({});
         if (!movies.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Movie not found` })
+            return res.status(404).json({ success: false, error: `Movie not found` })
         }
         return res.status(200).json({ success: true, data: movies })
-    }).catch(err => console.log(err))
+    } catch (err) {
+        return res.status(400).json({ success: false, error: err })
+    }
 }
 
 module.exports = {
