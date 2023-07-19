8571a99e8e23fe13d14c14f7241ad98b067e84a1
Thomas.N <thomasnolasque@cmail.carleton.ca>
2023-07-19 12:47:12 -0400

    Fullstack works locally
    Implemented MoviesList.jsx
    Next step is testing on live hosting server.

--- react-frontend/vite-project/src/pages/MoviesList.jsx
+++ react-frontend/vite-project/src/pages/MoviesList.jsx
@@ -1,11 +1,82 @@
 import React, { Component } from 'react'
 
+import ReactTable from 'react-table-6'
+import 'react-table-6/react-table.css'
+
+import api from '../api'
+
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    padding: 0 40px 40px 40px;
+`
+
 class MoviesList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: [],
+            columns: [],
+            isLoading: false,
+        }
+    }
+
+    componentDidMount = async () => {
+        this.setState({ isLoading: true })
+
+        await api.getAllMovies().then(movies => {
+            this.setState({
+                movies: movies.data.data,
+                isLoading: false,
+            })
+        })
+    }
+
     render() {
+        const { movies, isLoading } = this.state
+        console.log('TCL: MoviesList -> render -> movies', movies)
+
+        const columns = [
+            {
+                Header: 'ID',
+                accessor: '_id',
+                filterable: true,
+            },
+            {
+                Header: 'Name',
+                accessor: 'name',
+                filterable: true,
+            },
+            {
+                Header: 'Rating',
+                accessor: 'rating',
+                filterable: true,
+            },
+            {
+                Header: 'Time',
+                accessor: 'time',
+                Cell: props => <span>{props.value.join(' / ')}</span>,
+            },
+        ]
+
+        let showTable = true
+        if (!movies.length) {
+            showTable = false
+        }
+
         return (
-            <div>
-                <p>Movies List</p>
-            </div>
+            <Wrapper>
+                {showTable && (
+                    <ReactTable
+                        data={movies}
+                        columns={columns}
+                        loading={isLoading}
+                        defaultPageSize={10}
+                        showPageSizeOptions={true}
+                        minRows={0}
+                    />
+                )}
+            </Wrapper>
         )
     }
 }
