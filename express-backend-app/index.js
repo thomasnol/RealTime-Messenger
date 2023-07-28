2010d7ed7e6f78af43d141f7d8794d60856bb1e5
Thomas.N <thomasnolasque@cmail.carleton.ca>
2023-07-28 16:55:04 -0400

    commented out MongoDB
    To setup in index.js backend:
    
    io.on('connection', (socket) => {
        console.log('connection is ready');
        socket.on('send-message', () => {
            console.log("message recieved")
            // io.emit('message-broadcast', msg)
        })
    })

--- express-backend-app/index.js
+++ express-backend-app/index.js
@@ -3,17 +3,19 @@ import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 const app = express()
+const PORT = 4000
 
 const httpServer = http.createServer(app)
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: ["http://localhost:3000"]
     }
 })
 
 import path from 'path'
 const __dirname = path.resolve()
 
+/*
 import mongoose from 'mongoose'
 // const bodyParser = require('body-parser')
 import cors from 'cors'
@@ -27,16 +29,7 @@ const corsOptions = {
 }
 app.use(express.json());
 app.use(cors(corsOptions));
-
-// connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 9000
-    httpServer.listen(PORT, () => {
-        console.log(`App is listening on http://localhost:${PORT}`);
-    })
-}).catch(err => {
-    console.log(err);
-});
+*/
 
 app.get('/', (req, res) => {
     //res.send('Hello World!')
@@ -46,4 +39,24 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('connection is ready');
+    socket.on('send-message', () => {
+        console.log("message recieved")
+        // io.emit('message-broadcast', msg)
+    })
+})
+
+/*
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const PORT = process.env.PORT || 9000
+    httpServer.listen(PORT, () => {
+        console.log(`App is listening on http://localhost:${PORT}`);
+    })
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+httpServer.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:4000`);
 })
--- react-frontend/vite-project/src/App.jsx
+++ react-frontend/vite-project/src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 import { useState, useEffect } from "react"
 import { io } from "socket.io-client"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Button from '@mui/material/Button';
-
-
 function App() {
   const [socket, setSocket] = useState(null)
+  const [message, setMessage] = useState("")
 
   useEffect(() => {
-    setSocket(io("http://localhost:8000"))
+    setSocket(io("http://localhost:4000"))
   },[])
+
+  const handleForm = (e) => {
+    e.preventDefault()
+    console.log(message)
+    socket.emit("send-message")
+    setMessage("")
+  }
 
   // Fetching message from backend
   // const [message, setMessage] = useState("")
@@ -25,8 +34,16 @@ function App() {
   //}, []);
 
   return (
-    <div> React Frontend
-      <Button variant="text">Text</Button>
+    <div>
+      <Box component="form" onSubmit={handleForm}>
+        <TextField
+          label="Type your message here"
+          variant="standard"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button variant="text" type="submit">Send</Button>
+      </Box>
     </div>
   )
 }
