598e4a1b7dbdf6ed8e4e19c2f25a1b47810958f6
Thomas.N <thomasnolasque@cmail.carleton.ca>
2023-08-13 19:02:47 -0400

    Fixed implementation of .env file

--- express-backend-app/index.js
+++ express-backend-app/index.js
@@ -4,10 +4,12 @@ import { Server } from 'socket.io'
 import sockets from './socket/sockets.js'
 import mongoose from 'mongoose'
 import path from 'path'
-const __dirname = path.resolve()
+
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT // should be 4000
 
 const httpServer = http.createServer(app)
 const io = new Server(httpServer, {
@@ -16,25 +18,25 @@ const io = new Server(httpServer, {
   }
 })
 
+// await mongoose.connect
+// 'mongodb://localhost:27017/chat-app'
 
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  httpServer.listen(PORT, () => {
+      console.log(`App is listening on http://localhost:${PORT}`);
+  })
+}).catch(err => {
+  console.log(err);
+});
+
+const __dirname = path.resolve()
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', sockets)
 
-/*
-// connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 9000
-    httpServer.listen(PORT, () => {
-        console.log(`App is listening on http://localhost:${PORT}`);
-    })
-}).catch(err => {
-    console.log(err);
-});
-*/
-
-httpServer.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:4000`)
-})
+// httpServer.listen(PORT, () => {
+//   console.log(`Server listening on http://localhost:4000`)
+// })
