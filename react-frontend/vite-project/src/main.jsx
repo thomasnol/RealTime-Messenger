a4ecf56d8974844ae7750f39f8befd8f1758172b
Thomas.N <thomasnolasque@cmail.carleton.ca>
2023-08-17 02:40:30 -0400

    Cleanup

--- react-frontend/vite-project/src/main.jsx
+++ react-frontend/vite-project/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './index.css'
-// import reportWebVitals from './reportWebVitals'
 import router from './router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
