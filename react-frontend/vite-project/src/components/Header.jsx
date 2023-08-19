57da83419b40ce74a7969ca697ff3891d6048272
Thomas.N <thomasnolasque@cmail.carleton.ca>
2023-08-19 05:02:08 -0400

    Added user's choice for room names

--- react-frontend/vite-project/src/components/Header.jsx
+++ react-frontend/vite-project/src/components/Header.jsx
@@ -6,16 +6,25 @@ import Card from '@mui/material/Card'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import { v4 as uuidv4 } from 'uuid'
+import { TextField } from '@mui/material'
 
 export default function Header({socket, userId, setUserId}) {
   const navigate = useNavigate()
   const [rooms, setRooms] = useState([])
   
   function createNewRoom() {
+    let newName = ""
+    const tName = document.getElementById("newRoomName").value.trim()
+    if ((tName.length < 31) && (tName.length > 3 )){
+      newName = tName
+    } else {newName = "Unnamed Room"}
+    document.getElementById('newRoomName').value = ""
+    console.log("newName:" + newName)
+
     const roomId = uuidv4()
     navigate(`/room/${roomId}`)
-    socket.emit('create-new-room', { name:'Test Room', roomId })
-    setRooms((prev) => [...prev, { name:'Room Test', roomId }])
+    socket.emit('create-new-room', { name:newName, roomId })
+    setRooms((prev) => [...prev, { name:newName, roomId }])
   }
 
   useEffect(() => {
@@ -56,39 +65,46 @@ export default function Header({socket, userId, setUserId}) {
   }
 
   return (
-    <Card sx={{marginTop: 3, backgroundColor: "gray"}} raised>
-      <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-        <Box>
-          <Link style={{ textDecoration: "none" }} to="/">
-            <Button sx={{color: "white"}} variant="text">Home</Button>
-          </Link>
-          {rooms.map((room) => (
-            <Link style={{ textDecoration: "none" }} to={`/room/${room.roomId}`} key={room._id}>
-              <Button sx={{color: "white"}} variant="text">
-                { room.name }
-              </Button>
+    <>
+      <Card sx={{marginTop: 3, backgroundColor: "gray"}} raised>
+        <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+          <Box>
+            <Link style={{ textDecoration: "none" }} to="/">
+              <Button sx={{color: "white"}} variant="text">Home</Button>
             </Link>
-          ))}
+            {rooms.map((room) => (
+              <Link style={{ textDecoration: "none" }} to={`/room/${room.roomId}`} key={room._id}>
+                <Button sx={{color: "white"}} variant="text">
+                  { room.name }
+                </Button>
+              </Link>
+            ))}
+          </Box>
+          <Box>
+            <Button sx={{color: "white"}} variant="text" onClick={createNewRoom}>
+              New Room
+            </Button>
+            { userId &&
+              <Button sx={{color: "white"}} variant="text" onClick={logout}>
+                Logout
+              </Button>
+            }
+            { !userId &&
+              <Button sx={{color: "white"}} variant="text" onClick={login}>
+                Login
+              </Button>
+            }
+            <Link style={{ textDecoration: "none" }} to="/about">
+              <Button sx={{color: "white"}} variant="text">About</Button>
+            </Link>
+          </Box>
         </Box>
+      </Card>
+      <Box sx={{display: "flex", justifyContent: "right"}}>
         <Box>
-          <Button sx={{color: "white"}} variant="text" onClick={createNewRoom}>
-            New Room
-          </Button>
-          { userId &&
-            <Button sx={{color: "white"}} variant="text" onClick={logout}>
-              Logout
-            </Button>
-          }
-          { !userId &&
-            <Button sx={{color: "white"}} variant="text" onClick={login}>
-              Login
-            </Button>
-          }
-          <Link style={{ textDecoration: "none" }} to="/about">
-            <Button sx={{color: "white"}} variant="text">About</Button>
-          </Link>
+          <TextField id="newRoomName" style={{margin: 2}} inputProps={{ maxLength: 30 }}></TextField>
         </Box>
       </Box>
-    </Card>
+    </>
   )
 }
